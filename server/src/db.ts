cc420cff0ed1c7f35d82a870a45a028a4365a2d2
adqm0001 <adam.benmoussa04@gmail.com>
2026-05-16 00:23:27 -0400

    fix: add SSL for Render postgres, update font

--- server/src/db.ts
+++ server/src/db.ts
@@ -2,5 +2,8 @@ import { PrismaClient } from '@prisma/client'
 import { PrismaPg } from '@prisma/adapter-pg'
 import 'dotenv/config'
 
-const adapter = new PrismaPg({connectionString: process.env.DATABASE_URL});
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
+  ssl: { rejectUnauthorized: false }
+});
 export const prisma = new PrismaClient({ adapter })
